07a0cd7b93e0de81d5cab62210ffea143224bd90
mildred.vittor@amalitech.org <mildred.vittor@amalitech.org>
2021-07-12 14:30:33 +0000

    quote-api

--- buildalibrary.js
+++ buildalibrary.js
@@ -60,8 +60,8 @@ class media{
       return this.runtime;
     }
   }
-  const historryOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything')
-  ,544),
+  const historryOfEverything = new Book('Bill Bryson','A Short History of Nearly Everything',544)
+  
   
   historyOfEverything.toggleCheckoutStatus();
   console.log(historyOfEverything.isCheckedout);
---
+++ quoteapi.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+app.use(express.static('public'));
+
+app.get('/api/quotes/random', (req, res, next) => {
+  res.send({ quote: getRandomElement(quotes)});
+});
+
+app.get('/api/quotes', (req, res, next) => {
+  if (!req.query.person) {
+    res.send({ quotes: quotes });
+    return;
+  }
+
+  const personalQuotes = quotes
+    .filter(quote => quote.person === req.query.person);
+
+  res.send({quotes: personalQuotes});
+})
+
+app.post('/api/quotes', (req, res, next) => {
+  if (!req.query.person || !req.query.quote) {
+    res.status(400).send();
+    return;
+  }
+
+  const newQuote = {
+    quote: req.query.quote,
+    person: req.query.person
+  }
+
+  quotes.push(newQuote);
+  res.status(201).send({ quote: newQuote });
+});
+
+app.listen(PORT);
+
+
